f7283fa609378a6947b25f4ef75c32dc2855fb1d
kwizera Gabin Armel <kwizeragabin921@gmail.com>
2026-02-28 17:38:56 +0200

    made changes to the settings.py file

--- rema_foundation/settings.py
+++ rema_foundation/settings.py
@@ -125,4 +125,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = BASE_DIR/"staticfiles"
