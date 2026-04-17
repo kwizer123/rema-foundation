d2f7ac299d3576bac33b876c04c6481545d7ceb6
kwizera Gabin Armel <kwizeragabin921@gmail.com>
2026-04-17 20:57:39 +0200

    made debug false to put into production

--- rema_foundation/settings.py
+++ rema_foundation/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8#&d%%&#p!4d@z#_jxwudn@*rcqyn@*o3&niff5h#=7(p1s)h!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['Gabin1234.pythonanywhere.com', 'localhost', '127.0.0.1']
 
